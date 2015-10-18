d9d204ef6382bf4d309fc5f213cd67aa852eb7ca
bleutruth <bleutruth@gmail.com>
2015-10-19 01:47:26 +0900

    mrb_open, mrb_closeを呼び出すようにしてみた。
    libmruby_core.libをリンクしたり、
    リンクエラーを避けるために
    mrb_init_mrbgems, mrb_init_mrblib関数を実装したりしている。

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 ﻿
+#include <mruby.h>
+
 #include <crtdbg.h>
 
 int main()
@@ -6,5 +8,21 @@ int main()
 	// Enable run-time memory check for debug builds.
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
+	auto const mrb = mrb_open();
+	mrb_close(mrb);
 	return 0;
 }
+
+extern "C"{
+
+//	mrb_open_allocf(state.c)から呼ばれる
+void mrb_init_mrbgems(mrb_state *mrb)
+{
+}
+
+//	mrb_init_core(init.c)から呼ばれる
+void mrb_init_mrblib(mrb_state *mrb)
+{
+}
+
+}	//	extern "C"
