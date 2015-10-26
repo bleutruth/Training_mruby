5de19d251edecf0ffb4826d854546a96e971d23a
bleutruth <bleutruth@gmail.com>
2015-10-27 00:39:03 +0900

    文字列のmrb_value生成を試す。

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,15 @@ int main()
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
 	auto const mrb = mrb_open();
-	auto const v = mrb_load_string(mrb, "'abcde'");
-	auto const type = mrb_type(v);
-	auto const s = RSTRING_PTR(v);
-	auto const len = RSTRING_LEN(v);
+
+//	auto const v = mrb_load_string(mrb, "'abcde'");
+//	auto const type = mrb_type(v);
+//	auto const s = RSTRING_PTR(v);
+//	auto const len = RSTRING_LEN(v);
+
+	auto const s0 = mrb_str_new_cstr(mrb, "abcde");
+	auto const s1 = mrb_str_new_lit(mrb, "abcde");
+
 	mrb_close(mrb);
 	return 0;
 }
