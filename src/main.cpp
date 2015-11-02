521d33ad8ad45ca57040296164b9ddb8d925814f
bleutruth <bleutruth@gmail.com>
2015-11-03 01:30:35 +0900

    test.rbを読むようにした。

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,33 @@
 
 #include <crtdbg.h>
 
+FILE* ExecFile(mrb_state& mrb, char filename[])
+{
+	auto const fp = fopen(filename, "r");
+	if(!fp){
+		return 0;
+	}
+	return fp;
+}
+
+void Exec(mrb_state& mrb, char filename[])
+{
+	auto const fp = ExecFile(mrb, filename);
+	if(fp){
+		fclose(fp);
+	}
+}
+
 int main()
 {
 	// Enable run-time memory check for debug builds.
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
 	auto const mrb = mrb_open();
-
-//	auto const v = mrb_load_string(mrb, "'abcde'");
-//	auto const type = mrb_type(v);
-//	auto const s = RSTRING_PTR(v);
-//	auto const len = RSTRING_LEN(v);
-
-	auto const s0 = mrb_str_new_cstr(mrb, "abcde");
-	auto const s1 = mrb_str_new_lit(mrb, "abcde");
-
+	if(!mrb){
+		return -1;
+	}
+	Exec(*mrb, "test.rb");
 	mrb_close(mrb);
 	return 0;
 }
